447667b4af8c5b742b0e47b4d98f30dc8263946c
WebRizen <webrizen@gmail.com>
2024-03-29 18:10:24 +0530

    updated Mobile View of Hero Section ✅✨

--- frontend/user/src/components/design/Hero.jsx
+++ frontend/user/src/components/design/Hero.jsx
@@ -395,35 +395,35 @@ export default function Hero() {
     ];
     return (
         <>
-<div className="flex flex-row items-center justify-center py-20 h-screen md:h-auto dark:bg-black bg-white relative w-full">
-      <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
-        <motion.div
-          initial={{
-            opacity: 0,
-            y: 20,
-          }}
-          animate={{
-            opacity: 1,
-            y: 0,
-          }}
-          transition={{
-            duration: 1,
-          }}
-          className="div"
-        >
-          <h1 className="text-center text-xl md:text-6xl font-bold text-black dark:text-white">
-          Ignite Engagement with SpinFlame
-          </h1>
-          <p className="text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-2xl mt-2 mx-auto">
-          Our platform offers creators an immersive space to host interactive events, spark excitement, and reward participation.
-          </p>
-        </motion.div>
-        <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
-        <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
-          <World data={sampleArcs} globeConfig={globeConfig} />;
-        </div>
-      </div>
-    </div>
+            <div className="flex flex-row items-center justify-center md:py-20 py-36 h-screen md:h-auto dark:bg-black bg-white relative w-full">
+                <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
+                    <motion.div
+                        initial={{
+                            opacity: 0,
+                            y: 20,
+                        }}
+                        animate={{
+                            opacity: 1,
+                            y: 0,
+                        }}
+                        transition={{
+                            duration: 1,
+                        }}
+                        className="div"
+                    >
+                        <h1 className="text-center text-4xl md:text-6xl font-bold text-black dark:text-white">
+                            Ignite Engagement with SpinFlame
+                        </h1>
+                        <p className="text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-2xl mt-2 mx-auto">
+                            Our platform offers creators an immersive space to host interactive events, spark excitement, and reward participation.
+                        </p>
+                    </motion.div>
+                    <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
+                    <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
+                        <World data={sampleArcs} globeConfig={globeConfig} />;
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
