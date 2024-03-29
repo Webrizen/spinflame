2bf3130121b7b23881e7418fe3bda2a15ee98a7f
WebRizen <webrizen@gmail.com>
2024-03-29 22:02:06 +0530

    Deployment error resolved for useSearchParams in react ✅

--- frontend/user/src/app/auth/verify-email/page.jsx
+++ frontend/user/src/app/auth/verify-email/page.jsx
@@ -4,7 +4,7 @@ import { useSearchParams, useRouter } from 'next/navigation';
 import axios from 'axios';
 import { useToast } from '@/components/ui/use-toast';
 
-export default function page() {
+function Verify() {
     const [isLoading, setIsLoading] = useState(true);
     const searchParams = useSearchParams();
     const token = searchParams.get('token');
@@ -37,31 +37,30 @@ export default function page() {
     }, [token]);
 
     return (
-        <Suspense>
-            <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="flex flex-col items-center justify-center min-h-screen">
             {isLoading ? (
                 <p className='flex flex-row items-center text-center text-xl'>
-                <svg
-                    className="animate-spin h-10 w-10 mr-4 text-red-500"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                >
-                    <circle
-                        className="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        strokeWidth="4"
-                    ></circle>
-                    <path
-                        className="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    ></path>
-                </svg>
-                verifying...
+                    <svg
+                        className="animate-spin h-10 w-10 mr-4 text-red-500"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                    >
+                        <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            strokeWidth="4"
+                        ></circle>
+                        <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                    </svg>
+                    verifying...
                 </p>
             ) : (
                 <div className="text-center">
@@ -73,6 +72,13 @@ export default function page() {
                 </div>
             )}
         </div>
-        </Suspense>
     );
+}
+
+export default function page() {
+    return (
+        <Suspense>
+            <Verify />
+        </Suspense>
+    )
 }
