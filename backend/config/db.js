c447bc663dc66d7deac931727d04f4db7ab89c2b
rajeshmn47 <rajeshmn47@gmail.com>
2024-04-25 17:44:03 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const username = process.env.MONGODB_USERNAME;
 const password = process.env.MONGODB_PASSWORD;
 
 module.exports = {
-    url: `mongodb+srv://${username}:${password}@spinflame.l6oc6.mongodb.net/backend`,
+    url: `mongodb+srv://${username}:${password}@cluster0.rbx730f.mongodb.net/backend`,
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true,
